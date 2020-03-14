d8a9d2de230c9818bc938841d30cb134442f38b8
Adif Dwi Maulana <adifdwimaulana@gmail.com>
2020-03-14 22:50:29 +0700

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <h2 className="title">COVID-19 Monitoring System using Mathdroid Covid API</h2>
+        </div>
+    )
+}
